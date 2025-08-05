0272c046e587a759a6dfa4ae0e6e0aadc6963aed
Kirill Krylov <38138392+Loriendil@users.noreply.github.com>
2025-08-05 20:57:51 +0300

    minor changes

--- MolecularBiology.py
+++ MolecularBiology.py
@@ -1,25 +1,5 @@
 import random
 
-# Генетический код для трансляции
-GENETIC_CODE = {
-    'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',
-    'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
-    'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',
-    'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
-    'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',
-    'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
-    'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
-    'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
-    'UAU': 'Y', 'UAC': 'Y', 'UAA': '*', 'UAG': '*',
-    'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
-    'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
-    'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
-    'UGU': 'C', 'UGC': 'C', 'UGA': '*', 'UGG': 'W',
-    'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
-    'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
-    'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'
-}
-
 class Strand:
     def __init__(self, name:str, content:str):
         self.name:str = name
@@ -95,4 +75,23 @@ class RnaMolecule:
 class Protein:
     def __init__(self, mol_rna:RnaMolecule):
         self.mol_rna = mol_rna
+        # Генетический код для трансляции
+        GENETIC_CODE = {
+            'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',
+            'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+            'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',
+            'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+            'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',
+            'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+            'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+            'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+            'UAU': 'Y', 'UAC': 'Y', 'UAA': '*', 'UAG': '*',
+            'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+            'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+            'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+            'UGU': 'C', 'UGC': 'C', 'UGA': '*', 'UGG': 'W',
+            'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+            'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
+            'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'
+        }
         pass
